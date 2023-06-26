4a89b59ba2b1e264d675a2ebc9bc6c4274bf6643
Soga Okusami <sogafsd@yahoo.com>
2023-06-26 06:06:10 +0100

    scrimba password generator solo project

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+//global variables
+let btnEl = document.getElementById("generate-btn");
+let spanEl = document.getElementById("password1");
+let spanEl2 = document.getElementById("password2");
+
+const password = (input) => {
+  let password = [];
+  let finalPassword;
+  for (let i = 0; i < 15; i++) {
+    // generate an array of randomly selected characters
+    password.push(characters[Math.floor(Math.random() * 90) + 1]);
+    finalPassword = password.join("");
+  }
+  input.textContent = finalPassword;
+};
+
+function clickHandler() {
+  password(spanEl);
+  password(spanEl2);
+}
+
+btnEl.addEventListener("click", clickHandler);
